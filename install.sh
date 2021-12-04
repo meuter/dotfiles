073f116681de143f117eb449a515ec7fa585601c
Cédric Meuter <cedric.meuter@gmail.com>
2021-12-04 12:13:03 +0000

    Now installing bat and exa

--- install.sh
+++ install.sh
@@ -33,12 +33,13 @@ function initrc() {
 function install_gnu_stow() {
     banner "Installing GNU/Stow"
     local version=${1-2.3.1}
-    curl -L http://ftp.gnu.org/gnu/stow/stow-${version}.tar.gz --output /tmp/stow-${version}.tar.gz
-    tar xvf /tmp/stow-${version}.tar.gz -C ${SRC}/
+    local tarball=stow-${version}.tar.gz
+    curl -L http://ftp.gnu.org/gnu/stow/${tarball} --output /tmp/${tarball}
+    tar xvf /tmp/${tarball} -C ${SRC}/
     pushd .
         cd ${SRC}/stow-${version}/
         ./configure --prefix ~/.local/ && make install
-        rm -rf /tmp/stow-${version}.tar.gz
+        rm -rf /tmp/${tarball}
     popd
     initrc 'export PERL5LIB=~/.local/share/perl/'
     initrc 'export PATH=~/.local/bin:$PATH'
@@ -64,6 +65,34 @@ function install_libtree() {
     initrc 'export PATH=~/.local/bin:$PATH'
 }
 
+function install_exa() {
+    banner "Installing exa"
+    local version=${1-v0.10.1}
+    local zipfile=exa-linux-x86_64-${version}.zip
+    curl -L https://github.com/ogham/exa/releases/download/${version}/${zipfile} --output /tmp/${zipfile}
+    pushd .
+        cd ~/.local
+        unzip -o /tmp/${zipfile}
+        rm -rf ${zipfile}
+    popd
+    initrc 'export PATH=~/.local/bin:$PATH'
+    initrc 'alias ls="exa --icons"'
+}
+
+function install_bat() {
+    banner "Installing bat"
+    local version=${1-v0.18.3}
+    local tarball=bat-${version}-x86_64-unknown-linux-musl.tar.gz
+    curl -L https://github.com/sharkdp/bat/releases/download/${version}/${tarball} --output /tmp/${tarball}
+    pushd .
+        cd /tmp
+        tar xvf /tmp/${tarball}
+        find /tmp/${tarball%.tar.gz} -name "bat" -type f -exec mv {} ~/.local/bin/ \;
+#        rm -rf ${tarball}
+    popd
+    initrc 'export PATH=~/.local/bin:$PATH'
+}
+
 function install_neovim() {
     banner "Installing NeoVIM"
     local version=${1-v0.6.0}
@@ -72,6 +101,7 @@ function install_neovim() {
     stow neovim
     initrc 'export PATH=~/.local/bin:$PATH'
     initrc 'alias vim=nvim'
+    
     local packer=~/.local/share/nvim/site/pack/packer/start/packer.nvim
     rm -rf ${packer}
     git clone --depth 1 https://github.com/wbthomason/packer.nvim ${packer}
@@ -82,4 +112,6 @@ function install_neovim() {
 install_gnu_stow
 configure_git
 install_libtree
+install_exa
+install_bat
 install_neovim
