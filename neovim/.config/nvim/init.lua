ee5042ae67e3ffe0de753b3ec410c77a8e79d589
Cédric Meuter <cedric.meuter@gmail.com>
2021-12-04 10:32:04 +0000

    improved config and added lualine

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
 require("defaults")
 require("plugins")
 
-local ok, _ = pcall(require, 'github-theme')
-if ok then
-	require("github-theme").setup()
-	vim.cmd("colorscheme github_dark")
-end
-
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,4 +1,25 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'projekt0n/github-nvim-theme'
+
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = {'kyazdani42/nvim-web-devicons', opt=true},
+    config = function()
+      require("lualine").setup({
+          theme='auto',
+          options = {section_separators = '', component_separators = ''}
+      })
+    end
+  }
+
+  use {
+    "projekt0n/github-nvim-theme",
+    after = "lualine.nvim",
+    config = function()
+      require("github-theme").setup({
+        theme_style = "dark"
+      })
+    end
+  }
+
 end)
