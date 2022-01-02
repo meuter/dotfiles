38e7bc36b0c75571a3316ec769c4838a1fc1b2fd
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-02 19:13:19 +0000

    configure treesitter for all languages by default

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,13 @@
+local load = require("load")
+
 return {
     'nvim-treesitter/nvim-treesitter',
+    config = load.and_setup("nvim-treesitter.configs") {
+	  ensure_installed = "all",
+	  highlight = {
+	    enable = true,
+	    disable = { },
+	  },
+    },
     run = ':TSUpdate'
 }
