a49530a9b4c4328130f1d8ee1f0a531c2c23ccc9
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-02 16:22:56 +0000

    Use load helper function

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,109 +1,89 @@
-function setup(module_name)
-    function setup_result(opts)
-        local ok, module = pcall(require, module_name)
-        if ok then
-            module.setup(opts)
-        end
-        if opts["configure"] then
-            opts.configure()
-        end
-    end
-    return setup_result
-end
+local load = require("load")
 
 return require('packer').startup(function(use)
 
-    -- Packer
     use 'wbthomason/packer.nvim'
 
-    -- Github theme
     use {
         "projekt0n/github-nvim-theme",
-        config = setup("github-theme") {
+        config = load.and_setup("github-theme") {
             theme_style = "dark"
         }
     }
 
-    -- Lualine
     use {
         "nvim-lualine/lualine.nvim",
         requires = {
             {'kyazdani42/nvim-web-devicons', opt=true},
         },
-        config = setup("lualine") {
-            options = {
-                theme='ayu_dark',
-                section_separators = '',
-                component_separators = ''
-            },
-            configure = function()
-                vim.cmd("set noshowmode laststatus=2")
-            end
-        }
+        config = load.and_configure("lualine", function(module)
+            module.setup {
+                options = {
+                    theme='ayu_dark',
+                    section_separators = '',
+                    component_separators = ''
+                },
+            }
+            vim.cmd("set noshowmode laststatus=2")
+        end)
+
     }
 
-    -- Treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
 
-    -- Telescope
     use {
         'nvim-telescope/telescope.nvim',
         requires = {
             {'nvim-lua/plenary.nvim'}
         },
-        config = setup("telescope") {
-            pickers = {
-                find_files = {
-                    hidden=true,
-                }
-            },
-            configure = function()
-                vim.api.nvim_set_keymap("n", "<C-p>", "<CMD>lua require('project-files').project_files()<CR>", {noremap = true, silent = true})
-            end
-        }
+        config = load.and_configure("telescope", function(module)
+            module.setup {
+                pickers = {
+                    find_files = {
+                        hidden=true,
+                    }
+                },
+            }
+            vim.api.nvim_set_keymap("n", "<C-p>", "<CMD>lua require('project-files').project_files()<CR>", {noremap = true, silent = true})
+        end)
     }
 
-    -- LSP
     use {
         'neovim/nvim-lspconfig',
-        config = function()
-            local ok, module = pcall(require, "lspconfig")
-            if ok then
-                module.pyright.setup {}
-            end
-        end
+        config = load.and_configure("lspconfig", function(module)
+            module.pyright.setup {}
+        end)
     }
 
-    use { 'hrsh7th/cmp-nvim-lsp' }
-    use { 'hrsh7th/cmp-buffer' }
-    use { 'hrsh7th/cmp-path' }
-    use { 'hrsh7th/cmp-cmdline' }
     use {
         'hrsh7th/nvim-cmp',
-        config = function()
-            local ok, cmp = pcall(require, "cmp")
-            if ok then
-                cmp.setup {
-                    mapping = {
-                        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                        ['<C-y>'] = cmp.config.disable,
-                        ['<C-e>'] = cmp.mapping({
-                            i = cmp.mapping.abort(),
-                            c = cmp.mapping.close(),
-                        }),
-                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    },
-                    sources = cmp.config.sources({
-                        { name = 'nvim_lsp' },
-                    }, {
-                        { name = 'buffer' },
-                    })
-                }
-            end
-        end
+        requires = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'hrsh7th/cmp-cmdline' },
+        },
+        config = load.and_configure("cmp", function(cmp)
+            cmp.setup {
+                mapping = {
+                    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+                    ['<C-y>'] = cmp.config.disable,
+                    ['<C-e>'] = cmp.mapping({
+                        i = cmp.mapping.abort(),
+                        c = cmp.mapping.close(),
+                    }),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                }, {
+                    { name = 'buffer' },
+                })
+            }
+        end)
     }
 
 end)
