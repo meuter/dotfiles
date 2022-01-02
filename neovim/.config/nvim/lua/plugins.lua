abaebc5f5b4545eb3ddc2fdcb55beb0184189c96
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-02 12:19:27 +0000

    nvim> add treesitter and telescope

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+function setup(module_name)
+    function setup_result(opts)
+        local ok, module = pcall(require, module_name)
+        if ok then
+            module.setup(opts)
+        end
+        if opts["configure"] then
+            opts.configure()
+        end
+    end
+    return setup_result
+end
+
+
 return require('packer').startup(function(use)
 
     -- Packer
@@ -6,14 +20,9 @@ return require('packer').startup(function(use)
     -- Github theme
     use {
         "projekt0n/github-nvim-theme",
-        config = function()
-            local ok, module = pcall(require, "github-theme")
-            if ok then
-                module.setup {
-                    theme_style = "dark"
-                }
-            end
-        end
+        config = setup("github-theme") {
+            theme_style = "dark"
+        }
     }
 
     -- Lualine
@@ -22,19 +31,40 @@ return require('packer').startup(function(use)
         requires = {
             {'kyazdani42/nvim-web-devicons', opt=true},
         },
-        config = function()
-            local ok, module = pcall(require, "lualine")
-            if ok then
+        config = setup("lualine") {
+            options = {
+                theme='ayu_dark',
+                section_separators = '',
+                component_separators = ''
+            },
+            configure = function()
                 vim.cmd("set noshowmode laststatus=2")
-                module.setup {
-                    options = {
-                        theme='ayu_dark',
-                        section_separators = '',
-                        component_separators = ''
-                    }
-                }
             end
-        end
+        }
+    }
+
+    -- Treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            {'nvim-lua/plenary.nvim'}
+        },
+        config = setup("telescope") {
+            pickers = {
+                find_files = {
+                    hidden=true,
+                }
+            },
+            configure = function()
+                vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope git_files<cr>", { silent=true})
+            end
+        }
     }
 
 end)
