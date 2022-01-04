ada2a6661d5095b9ce3ee39be8775d250abf110b
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-04 18:12:49 +0000

    Improved git key bindings
    Configured ctrl-g to close vim fugitive and cc to close fugitive and
    open commit buffer

--- neovim/.config/nvim/lua/nvcode/keymaps.lua
+++ neovim/.config/nvim/lua/nvcode/keymaps.lua
@@ -3,6 +3,10 @@ local function noremap(mode, key, command)
     return vim.api.nvim_set_keymap(mode, key, command, options)
 end
 
+local function remap_for_filetype(filetype, mode, key, command)
+    vim.cmd("autocmd FileType " .. filetype .. " " .. mode .. "map <silent><buffer> " .. key .. " " .. command)
+end
+
 local function remap_all_modes(key, command)
     noremap("n", key, command)
     noremap("i", key, "<C-\\><C-N>" .. command)
@@ -17,6 +21,8 @@ remap_all_modes("<C-t>", "<CMD>Telescope buffers<CR>")
 
 -- ctrl+g to open git fugitive
 remap_all_modes("<C-g>", "<CMD>Git<CR>")
+remap_for_filetype("fugitive", "n", "<C-G>", "<CMD>q<CR>")
+remap_for_filetype("fugitive", "n", "cc", "<CMD>q<bar>Git commit --quiet<CR>")
 
 -- ctrl+b to toggle file explorer
 remap_all_modes("<C-b>", "<CMD>NvimTreeToggle<CR>")
