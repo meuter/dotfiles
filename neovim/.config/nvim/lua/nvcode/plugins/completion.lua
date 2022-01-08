68bcd1158ec1574321f3e1afa49d6f85f9923597
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-08 10:35:42 +0000

    Improved diagnostics configuration
    - not mode pesky shadow/virtual warnings displayed bu default
    - better icon in gutter for warnings and errors

--- neovim/.config/nvim/lua/nvcode/plugins/completion.lua
+++ neovim/.config/nvim/lua/nvcode/plugins/completion.lua
@@ -26,6 +26,37 @@ local function configure_lsp()
     local ok, lspconfig = pcall(require, "lspconfig")
     if not ok then return end
 
+    local signs = {
+        { name = "DiagnosticSignError", text = "" },
+        { name = "DiagnosticSignWarn", text = "" },
+        { name = "DiagnosticSignHint", text = "" },
+        { name = "DiagnosticSignInfo", text = "" },
+    }
+
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    vim.diagnostic.config {
+        -- disable virtual text
+        virtual_text = false,
+        -- show signs
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
+
     lspconfig.pyright.setup {}
 end
 
