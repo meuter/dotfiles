50a4315fda0021b892a52e9214ab43d86d813444
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-08 11:25:15 +0000

    Configured the formatting of the completion menu

--- neovim/.config/nvim/lua/nvcode/plugins/completion.lua
+++ neovim/.config/nvim/lua/nvcode/plugins/completion.lua
@@ -4,6 +4,34 @@ local function configure_nvim_cmp()
     local ok, cmp = pcall(require, "cmp")
     if not ok then return end
 
+    local kind_icons = {
+        Text = "",
+        Method = "m",
+        Function = "",
+        Constructor = "",
+        Field = "",
+        Variable = "",
+        Class = "",
+        Interface = "",
+        Module = "",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "",
+        Keyword = "",
+        Snippet = "",
+        Color = "",
+        File = "",
+        Reference = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+        Event = "",
+        Operator = "",
+        TypeParameter = "",
+    }
+
     cmp.setup {
         mapping = {
             ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -18,7 +46,20 @@ local function configure_nvim_cmp()
             { name = "nvim_lsp" },
         }, {
             { name = "buffer" },
-        })
+        }),
+        formatting = {
+            fields = { "kind", "abbr", "menu" },
+            format = function(entry, vim_item)
+                vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                vim_item.menu = ({
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[Snippet]",
+                    buffer = "[Buffer]",
+                    path = "[Path]",
+                })[entry.source.name]
+                return vim_item
+            end,
+        },
     }
 end
 
