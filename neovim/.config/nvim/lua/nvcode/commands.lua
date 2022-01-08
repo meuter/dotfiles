e3c310fbb1399149fa6f61b61cf0d6125701782f
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-08 18:03:40 +0000

    Added convenience commands to invoke telescope git

--- neovim/.config/nvim/lua/nvcode/commands.lua
+++ neovim/.config/nvim/lua/nvcode/commands.lua
@@ -2,3 +2,8 @@ vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 vim.cmd [[ command! Close execute 'Bd' ]]
 vim.cmd [[ command! CloseAll execute ':bufdo Bd' ]]
 vim.cmd [[ command! CloseAllExceptCurrent execute '%bd|e#|bd#' ]]
+
+vim.cmd [[ command! GitHistory execute 'Telescope git_commits' ]]
+vim.cmd [[ command! GitFileHistory execute 'Telescope git_bcommits' ]]
+vim.cmd [[ command! GitBranch execute 'Telescope git_branches' ]]
+vim.cmd [[ command! GitStatus execute 'Telescope git_status' ]]
