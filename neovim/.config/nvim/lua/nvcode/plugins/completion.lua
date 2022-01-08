87a7f6fddb90ebb05a86add48acf15343197af95
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-08 11:01:47 +0000

    Replaced single quotes by double quotes as it should be

--- neovim/.config/nvim/lua/nvcode/plugins/completion.lua
+++ neovim/.config/nvim/lua/nvcode/plugins/completion.lua
@@ -6,18 +6,18 @@ local function configure_nvim_cmp()
 
     cmp.setup {
         mapping = {
-            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-            ['<C-y>'] = cmp.config.disable,
-            ['<C-e>'] = cmp.mapping({
+            ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+            ["<C-y>"] = cmp.config.disable,
+            ["<C-e>"] = cmp.mapping({
                 i = cmp.mapping.abort(),
                 c = cmp.mapping.close(),
             }),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
         },
         sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
+            { name = "nvim_lsp" },
         }, {
-            { name = 'buffer' },
+            { name = "buffer" },
         })
     }
 end
@@ -62,18 +62,18 @@ end
 
 function M.startup(use)
     use {
-        'hrsh7th/nvim-cmp',
+        "hrsh7th/nvim-cmp",
         requires = {
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-cmdline' },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-cmdline" },
         },
         config = configure_nvim_cmp()
     }
 
     use {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         config = configure_lsp
     }
 
