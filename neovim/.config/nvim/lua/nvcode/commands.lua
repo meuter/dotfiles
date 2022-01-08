dd5f132a2fa74c6547aebc9d87724e6e68d42a77
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-08 17:46:49 +0000

    Added commands "Close", "CloseAll" and "CloseAllExceptCurrent"

--- neovim/.config/nvim/lua/nvcode/commands.lua
+++ neovim/.config/nvim/lua/nvcode/commands.lua
@@ -1,1 +1,4 @@
 vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+vim.cmd [[ command! Close execute 'Bd' ]]
+vim.cmd [[ command! CloseAll execute ':bufdo Bd' ]]
+vim.cmd [[ command! CloseAllExceptCurrent execute '%bd|e#|bd#' ]]
