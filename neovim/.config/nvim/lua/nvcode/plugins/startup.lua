8d6ac3c2d968b81ce8f1f68b7daf2cc715e1f07a
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-22 19:04:02 +0100

    Cleanup

--- neovim/.config/nvim/lua/nvcode/plugins/startup.lua
+++ neovim/.config/nvim/lua/nvcode/plugins/startup.lua
@@ -27,11 +27,7 @@ local function startup(use)
     comment.startup(use)
 
     --   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-    --   use "ahmedkhalf/project.nvim"
     --   use "lewis6991/impatient.nvim"
-    --   use "lukas-reineke/indent-blankline.nvim"
-    --   use "goolord/alpha-nvim"
-    --   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
     --   use "folke/which-key.nvim"
 
     --   -- LSP
