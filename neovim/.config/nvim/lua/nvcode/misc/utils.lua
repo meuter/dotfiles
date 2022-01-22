2857729039f7d42343ff24b0fcabf289a49a35e2
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-22 18:29:51 +0100

    Refactored the text editing to use LSP TextEdits

--- neovim/.config/nvim/lua/nvcode/misc/utils.lua
+++ neovim/.config/nvim/lua/nvcode/misc/utils.lua
@@ -1,19 +1,55 @@
 M = {}
 
--- TODO(cme): refactor this a but, there is a lot of copy-pasta
+function M.get_selected_range()
+    local range = {}
+    range.start_row = vim.fn.line("'<")
+    range.start_col = vim.fn.col("'<")
+    range.end_row = vim.fn.line("'>")
+    range.end_col = vim.fn.col("'>")
+    return range
+end
+
+function M.get_current_line_range()
+    local current_position = vim.api.nvim_win_get_cursor(0)
+    local current_line_index = current_position[1] -- lua table are 1-indexed!
+    local current_line = M.get_current_line()
+
+    local range = {}
+    range.start_row = current_line_index
+    range.start_col = 1
+    range.end_row = current_line_index
+    range.end_col = #current_line
+    return range
+end
+
+function M.convert_range_to_lsp_range(range)
+    return {
+        ["start"] = {
+            line = range.start_row - 1,
+            character = range.start_col - 1,
+        },
+        ["end"] = {
+            line = range.end_row - 1,
+            character = range.end_col,
+        },
+    }
+end
+
+function M.get_selected_lsp_range()
+    return M.convert_range_to_lsp_range(M.get_selected_range())
+end
+
+function M.get_current_line_lsp_range()
+    return M.convert_range_to_lsp_range(M.get_current_line_range())
+end
+
 function M.get_selected_lines()
-    local start_row = vim.fn.line("'<")
-    local start_col = vim.fn.col("'<")
-    local end_row = vim.fn.line("'>")
-    local end_col = vim.fn.col("'>")
-    local buffer = 0
-    local text = vim.api.nvim_buf_get_lines(buffer, start_row-1, end_row, false)
+    local range = M.get_selected_range()
+    local text = vim.api.nvim_buf_get_lines(0, range.start_row-1, range.end_row, false)
     local text_length = #text
-
-    end_col = math.min(#text[text_length], end_col)
-
+    local end_col = math.min(#text[text_length], range.end_col)
     local end_idx = vim.str_byteindex(text[text_length], end_col)
-    local start_idx = vim.str_byteindex(text[1], start_col)
+    local start_idx = vim.str_byteindex(text[1], range.start_col)
 
     text[text_length] = text[text_length]:sub(1, end_idx)
     text[1] = text[1]:sub(start_idx)
@@ -38,40 +74,24 @@ function M.get_current_mode()
     return vim.api.get_selected_lines()
 end
 
-function M.print_selected_lines()
-    local text = M.get_selected_lines()
-    print(vim.inspect(text))
-end
-
 function M.replace_current_line_with(lines_to_insert)
-    local current_position = vim.api.nvim_win_get_cursor(0)
-    local current_line_index = current_position[1] -- lua table are 1-indexed!
-    vim.api.nvim_buf_set_lines(0, current_line_index-1, current_line_index, true, lines_to_insert)
+    local edits = {
+        {
+            range = M.get_current_line_lsp_range(),
+            newText = table.concat(lines_to_insert, "\n")
+        }
+    }
+    vim.lsp.util.apply_text_edits(edits, 0, "utf-16")
 end
 
 function M.replace_selection_with(lines_to_insert)
-    local start_row = vim.fn.line("'<")
-    local start_col = vim.fn.col("'<")
-    local end_row = vim.fn.line("'>")
-    local end_col = vim.fn.col("'>")
-    local range = {
-        ["start"] = {
-            line = start_row - 1,
-            character = start_col - 1,
-        },
-        ["end"] = {
-            line = end_row - 1,
-            character = end_col,
-        },
+    local edits = {
+        {
+            range = M.get_selected_lsp_range(),
+            newText = table.concat(lines_to_insert, "\n")
+        }
     }
-    local text = table.concat(lines_to_insert, "")
-    local text_edit = {
-        range = range,
-        newText = text
-    }
-    local buffer = 0
-    local edit_set = { text_edit }
-    vim.lsp.util.apply_text_edits(edit_set, buffer, "utf-16")
+    vim.lsp.util.apply_text_edits(edits, 0, "utf-16")
 end
 
 return M
