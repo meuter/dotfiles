3d16b2ac69f86779d61b2eea53966bdf4b87807c
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-24 06:54:42 +0000

    Slightly improved the search and replace prompt

--- neovim/.config/nvim/lua/nvcode/misc/searchreplace.lua
+++ neovim/.config/nvim/lua/nvcode/misc/searchreplace.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.search_and_replace()
-    local text_to_replace = vim.fn.input("Replace> ")
+    local text_to_replace = vim.fn.input("Replace: ")
     if text_to_replace == "" then return end
 
     vim.cmd("vimgrep /" .. text_to_replace .. "/gj **/*")
-    local replacement = vim.fn.input("With> ");
+    local replacement = vim.fn.input("Replace '" .. text_to_replace .. "' with: ");
     if replacement == "" then return end
 
     vim.cmd("redraw")
