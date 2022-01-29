b865564d9e5eb8c89988f6e2b587ad670929f71e
Cédric Meuter <cedric.meuter@gmail.com>
2022-01-29 20:20:13 +0100

    ctrl+h is apparently the same as C-Backspace

--- neovim/.config/nvim/lua/nvcode/keymaps.lua
+++ neovim/.config/nvim/lua/nvcode/keymaps.lua
@@ -48,9 +48,6 @@ remap_for_filetype("fugitive", "n", "<C-G>", "<CMD>q<CR>")
 remap_for_filetype("fugitive", "n", "<esc><esc>", "<CMD>q<CR>")
 remap_for_filetype("fugitive", "n", "cc", "<CMD>q<bar>Git commit --quiet<CR>")
 
--- ctrl+h for global search/replace
-remap_all_modes("<C-h>", "<CMD>SearchAndReplace<CR>")
-
 -- ctrl-[or] to navigate buffer
 remap_all_modes("<C-PageUp>", "<CMD>BufferLineCyclePrev<CR>")
 remap_all_modes("<C-PageDown>", "<CMD>BufferLineCycleNext<CR>")
