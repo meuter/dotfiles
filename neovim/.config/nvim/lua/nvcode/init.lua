922067211ad204608091c285b33ec7162f52350a
Cédric Meuter <cedric.meuter@gmail.com>
2022-06-06 13:12:20 +0200

    Added todo

--- neovim/.config/nvim/lua/nvcode/init.lua
+++ neovim/.config/nvim/lua/nvcode/init.lua
@@ -2,3 +2,8 @@ require("nvcode.options")
 require("nvcode.keymaps")
 require("nvcode.plugins")
 require("nvcode.commands")
+
+-- TODO:
+-- replace vim-fugitive with neogit (for integration with diffview)
+-- checout nvim-bqf for search and replace ??
+--
