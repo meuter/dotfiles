ddaedad9a6e195af860ed6ba3cdbb42db6f57149
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-09 17:01:20 +0100

    cleanup bat package

--- bat/package.sh
+++ bat/package.sh
@@ -8,14 +8,13 @@ function install_package() {
     local version=0.24.0
     local tarball=bat-v${version}-x86_64-unknown-linux-musl.tar.gz
     curl -L https://github.com/sharkdp/bat/releases/download/v${version}/${tarball} --output /tmp/${tarball}
-    cd /tmp
-    tar xvf /tmp/${tarball}
-    find /tmp/${tarball%.tar.gz} -name "bat" -type f -exec mv {} ~/.local/bin/ \;
+    tar xvf /tmp/${tarball} -C /tmp
+    find /tmp/${tarball%.tar.gz} -name "bat" -type f -exec mv {} ${DOTFILES_BIN}/ \;
     rm -rf /tmp/${tarball}
 }
 
 function uninstall_package() {
-    echo -n
+    rm -fv ${DOTFILES_BIN}/bat
 }
 
 function init_package() {
