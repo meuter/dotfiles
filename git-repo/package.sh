29452f2fa369c93827b8c4dfe736fc741b47d587
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-09 16:59:33 +0100

    added package for git-repo

---
+++ git-repo/package.sh
@@ -0,0 +1,18 @@
+#!/bin/false "This script should be sourced in a shell, not executed directly"
+
+function dependencies() {
+    echo ""
+}
+
+function install_package() {
+    curl https://storage.googleapis.com/git-repo-downloads/repo > ${DOTFILES_BIN}/repo
+    chmod a+rx ${DOTFILES_BIN}/repo
+}
+
+function uninstall_package() {
+    rm -fv ${DOTFILES_BIN}/repo
+}
+
+function init_package() {
+    echo -n
+}
