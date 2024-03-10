0e3c52e09c0a1220f3132753bea4611c3bac2e91
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 14:35:05 +0100

    final pass: sdkman

--- sdkman/package.sh
+++ sdkman/package.sh
@@ -1,20 +1,25 @@
-#!/bin/false "This script should be sourced in a shell, not executed directly"
-
 function install_package() {
     local script="/tmp/sdkman_bootstrap.sh"
     curl -L https://get.sdkman.io/ --output ${script}
     chmod u+x ${script}
-    SDKMAN_DIR=~/.local/sdkman SDKMAN_DIR_RAW=$SDKMAN_DIR ${script}
+    SDKMAN_DIR=${DOTFILES_PREFIX}/sdkman SDKMAN_DIR_RAW=${SDKMAN_DIR} ${script}
 }
 
 function uninstall_package() {
-    # TODO
-    echo ""
+    rm -rvf ${DOTFILES_PREFIX}/sdkman
 }
 
 function init_package() {
     set +u
-    export SDKMAN_DIR=~/.local/sdkman
-    export SDKMAN_DIR_RAW=~/.local/sdkman
-    source ~/.local/sdkman/bin/sdkman-init.sh
+    export SDKMAN_DIR=${DOTFILES_PREFIX}/sdkman
+    export SDKMAN_DIR_RAW=${DOTFILES_PREFIX}/sdkman
+    source ${SDKMAN_DIR}/bin/sdkman-init.sh
+}
+
+function uninit_package() {
+    unset SDKMAN_CANDIDATES_API
+    unset SDKMAN_CANDIDATES_DIR
+    unset SDKMAN_DIR
+    unset SDKMAN_DIR_RAW
+    unset SDKMAN_PLATFORM
 }
