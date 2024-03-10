ee0ee39c5ef19e75dcd26aee4764265cdf3eac6a
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 14:05:06 +0100

    final pass: git-delta

--- git-delta/package.sh
+++ git-delta/package.sh
@@ -3,28 +3,23 @@ function install_package() {
     local tarball=delta-${version}-x86_64-unknown-linux-musl.tar.gz
     local checksum=0a88c858c23149f246addab1403aa98f
 
-    # grab tarball
     curl -L https://github.com/dandavison/delta/releases/download/${version}/${tarball} --output /tmp/${tarball}
-
-    # check tarball
     if [ "${checksum}" != $(md5sum /tmp/${tarball} | awk '{print $1}') ]; then
         >&2 echo Unexpected checksum
         return 1
     fi
 
-    # extract tarball
-    tar xvf /tmp/${tarball} -C /tmp
+    mkdir -p /tmp/delta-${version}/
+    tar xvf /tmp/${tarball} -C /tmp/delta-${version}
 
-    # install binary
-    find /tmp/${tarball%.tar.gz} -name "delta" -type f -exec mv {} ${DOTFILES_BIN}/ \;
+    find /tmp/delta-${version} -name "delta" -type f -exec mv {} ${DOTFILES_BIN}/ \;
 
-    # remove tarball and extracted folder
-    rm -vf /tmp/${tarball}
-    rm -rvf /tmp/${tarball%.tar.gz}
+    rm -rv \
+        /tmp/${tarball} \
+        /tmp/delta-${version}
 }
 
 function uninstall_package() {
-    # remove binary
     rm -fv ${DOTFILES_BIN}/delta
 }
 
