5f67457f13d3a35f72234f7f0e9cf814c67da54b
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 15:16:36 +0100

    added shell-essential package

---
+++ shell-essential/package.sh
@@ -0,0 +1,13 @@
+function dependencies() {
+    echo "git-delta git-config"
+    echo "inputrc starship"
+    echo "bat exa fzf zoxide ripgrep starship"
+}
+
+function install_package() {
+    echo "Nothing to install"
+}
+
+function uninstall_package() {
+    echo "Nothing to uninstall"
+}
