f4e1a742b105a81d7f1f12cbca32fa3d373228bf
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 14:17:59 +0100

    final pass: ripgrep

--- ripgrep/package.sh
+++ ripgrep/package.sh
@@ -1,29 +1,23 @@
 function install_package() {
-    # version info
     local version=14.1.0
     local tarball=ripgrep-${version}-x86_64-unknown-linux-musl.tar.gz
     local checksum=ed206f245868d3df58d37599a811bf54
 
-    # grab tarball
     curl -L https://github.com/BurntSushi/ripgrep/releases/download/${version}/${tarball} --output /tmp/${tarball}
-
-    # check tarball
     if [ "${checksum}" != $(md5sum /tmp/${tarball} | awk '{print $1}') ]; then
         >&2 echo Unexpected checksum
         return 1
     fi
 
-    # extract tarball
-    tar xvf /tmp/${tarball} -C /tmp
+    mkdir -p /tmp/ripgrep-${version}
+    tar xvf /tmp/${tarball} -C /tmp/ripgrep-${version}
 
-    # install content
-    find /tmp/${tarball%.tar.gz} -name "rg" -type f -exec mv {} ${DOTFILES_BIN}/ \;
-    find /tmp/${tarball%.tar.gz} -name "rg.bash" -type f -exec mv {} ${DOTFILES_COMPLETION}/ \;
-    find /tmp/${tarball%.tar.gz} -name "rg.1" -type f -exec mv {} ${DOTFILES_MAN1} \;
+    find /tmp/ripgrep-${version} -name "rg"      -type f -exec mv {} ${DOTFILES_BIN}/ \;
+    find /tmp/ripgrep-${version} -name "rg.bash" -type f -exec mv {} ${DOTFILES_COMPLETION}/ \;
+    find /tmp/ripgrep-${version} -name "rg.1"    -type f -exec mv {} ${DOTFILES_MAN1} \;
 
-    # cleanup tarball and extracted archive
     rm -v /tmp/${tarball}
-    rm -rv /tmp/${tarball%.tar.gz}
+    rm -rv /tmp/ripgrep-${version}/
 }
 
 function uninstall_package() {
