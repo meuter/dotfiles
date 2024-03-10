ad7c8f64a73b7dde69df202ea9d1d44257600838
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 14:09:21 +0100

    final pass neovim

--- neovim/package.sh
+++ neovim/package.sh
@@ -1,25 +1,19 @@
 function install_package() {
-    # version info
     local version=0.9.5
     local tarball=nvim-linux64.tar.gz
     local checksum=9edfa4d46fc382ca90e8c4c84c2e8c73
 
-    # download tarball
     curl -L https://github.com/neovim/neovim/releases/download/v${version}/${tarball} --output /tmp/${tarball}
-
-    # check tarball
     if [ "${checksum}" != $(md5sum /tmp/${tarball} | awk '{print $1}') ]; then
         >&2 echo Unexpected checksum
         return 1
     fi
 
-    # extract tarball
     tar xvf /tmp/${tarball} --strip-components=1 -C ${DOTFILES_PREFIX}
     rm -rf /tmp/${tarball}
 }
 
 function uninstall_package() {
-    # remove all nvim files
     rm -rvf \
         ${DOTFILES_SHARE}/nvim/ \
         ${DOTFILES_BIN}/nvim \
