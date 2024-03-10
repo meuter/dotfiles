300d9e5474152b65a7957b9ddbabf1a911c2bc10
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-10 14:42:49 +0100

    final pass:zoxide

--- zoxide/package.sh
+++ zoxide/package.sh
@@ -1,34 +1,21 @@
 function install_package() {
-    # version info
     local version=0.9.4
     local tarball=zoxide-${version}-x86_64-unknown-linux-musl.tar.gz
     local checksum=7ca4e6c5ceb98bd1d5d8efb8460b640e
 
-    # grab tarball
     curl -L https://github.com/ajeetdsouza/zoxide/releases/download/v${version}/${tarball} --output /tmp/${tarball}
-
-    # check tarball
     if [ "${checksum}" != $(md5sum /tmp/${tarball} | awk '{print $1}') ]; then
         >&2 echo Unexpected checksum
         return 1
     fi
 
-    # extract tarball
     mkdir -p /tmp/zoxide-${version}
     tar xvf /tmp/${tarball} -C /tmp/zoxide-${version}
 
-    # install binary
-    mv -vf /tmp/zoxide-${version}/zoxide ${DOTFILES_BIN}/
+    mv -vf /tmp/zoxide-${version}/zoxide                    ${DOTFILES_BIN}/
+    mv -vf /tmp/zoxide-${version}/completions/zoxide.bash   ${DOTFILES_COMPLETION}/
+    mv -vf /tmp/zoxide-${version}/man/man1/*                ${DOTFILES_MAN1}/
 
-    # install bash completion
-    mkdir -p ${DOTFILES_SHARE}/zoxide/completion/
-    mv -vf /tmp/zoxide-${version}/completions/zoxide.bash ${DOTFILES_SHARE}/zoxide/completion/
-
-    # install manpage
-    mkdir -p ${DOTFILES_MAN}/man1/
-    mv -vf /tmp/zoxide-${version}/man/man1/* ${DOTFILES_MAN}/man1/
-
-    # cleanup tarball and extracted archive
     rm -v /tmp/${tarball}
     rm -rv /tmp/zoxide-${version}
 }
@@ -36,11 +23,11 @@ function install_package() {
 function uninstall_package() {
     rm -vf \
         ${DOTFILES_BIN}/zoxide \
-        ${DOTFILES_SHARE}/zoxide/completion/zoxide.bash \
-        ${DOTFILES_MAN}/man1/zoxide*.1
+        ${DOTFILES_COMPLETION}/zoxide.bash \
+        ${DOTFILES_MAN1}/zoxide*.1
 }
 
 function init_package() {
-    source ${DOTFILES_SHARE}/zoxide/completion/zoxide.bash
+    source ${DOTFILES_COMPLETION}/zoxide.bash
     eval "$(zoxide init --cmd cd bash)"
 }
