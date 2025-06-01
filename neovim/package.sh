3e64958d2917fea59276092106621cd5c2f1c69b
Cédric Meuter <cedric.meuter@gmail.com>
2025-06-01 06:20:22 +0000

    upgrade neovim to 0.11.2

--- neovim/package.sh
+++ neovim/package.sh
@@ -1,9 +1,9 @@
 function install_package() {
-    local version=0.10.2
-    local tarball=nvim-linux64.tar.gz
-    local checksum=d46c59493f415a41db14ba61286f8d9f
-    
-    curl -L https://github.com/neovim/neovim/releases/download/v${version}/${tarball} --output /tmp/${tarball}
+    local version=0.11.2
+    local tarball=nvim-linux-x86_64.tar.gz
+    local checksum=c800a7f591da60c4dc492a17381051af
+
+    curl --fail -L https://github.com/neovim/neovim/releases/download/v${version}/${tarball} --output /tmp/${tarball}
     if [ "${checksum}" != $(md5sum /tmp/${tarball} | awk '{print $1}') ]; then
         >&2 echo Unexpected checksum
         return 1
